dc9e8e10b3f8e9359c67c82589ceb9d92ec86f5e
Abhijit Parlapalli <abhijit.parla01@gmail.com>
2023-09-13 14:43:09 +0530

    Update signup.test.js

--- tests/signup.test.js
+++ tests/signup.test.js
@@ -3,17 +3,12 @@ const server = require('../server.js')
 const request = require('express')
 
 test('should accept userid and password and should signup succeessfully',  () => {
-    // const adminObj = {
-    //     username:'khushboosuckmydick@gmail.com',
-    //     password:'jameserrr@2023'
-    // }
     expect(Index(2,5)).toBe(7)
 
     describe('username and password', () => {
         test("should have username", async () => {
             const response = await request(server).post('/admin/signup').send({
-                username: "username",
-                password:"password"
+                
             })
             expect(response.statusCode).toBe(200)
         })
